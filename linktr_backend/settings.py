9b188dd60eb37d37105ad52007e416ce04e0df52
kguykrfghdcgresdrg <120589608+kguykrfghdcgresdrg@users.noreply.github.com>
2025-02-27 12:40:06 +0530

    Changes in settings.py

--- linktr_backend/settings.py
+++ linktr_backend/settings.py
@@ -104,8 +104,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dineshrbalaji@gmail.com'
-EMAIL_HOST_PASSWORD = 'vian fwzo qnuu kbeo'
+EMAIL_HOST_USER = 'yourgmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
 
 
 # Password validation
